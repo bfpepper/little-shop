b1a089a7de0569eab689f6453fa05c7115ca3194
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-09-14 17:46:11 -0600

    Create order item model with relationships

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
+  has_many :order_items
+  has_many :items, through: :order_items
 
 end
---
+++ app/models/order_item.rb
@@ -0,0 +1,5 @@
+class OrderItem < ApplicationRecord
+  belongs_to :orders
+  belongs_to :items
+
+end
