ae8829b6898c06871099164c1a5078d53f5631ba
Ben Pepper <bfpepper87@gmail.com>
2016-09-14 16:25:48 -0600

    fix create account test to remove comments.

--- spec/features/guest_can_create_account_spec.rb
+++ spec/features/guest_can_create_account_spec.rb
@@ -3,57 +3,37 @@ require 'rails_helper'
 RSpec.feature "Visitor can get to new account page" do
   scenario "they can get to new account page" do
 
-    # When I visit "/"
     visit '/'
 
-    # Then I should see a link for "Login"
     expect(page).to have_content("Login")
 
-    # And when I click "Login"
     click_link "Login"
 
-    # And I should be on the "/login" page
     expect(current_path).to eq(login_path)
 
-    # I should see a place to insert my credentials to login
     find("input[placeholder='Username']")
     find("input[placeholder='Password']")
 
-    # And I should see a link to "Create Account"
     expect(page).to have_content("Create Account")
-    # page.should have_selector("input[type=submit][value='Create Account']")
   end
 
   scenario "Visitor can create a new account" do
 
-    # When I visit "/login
     visit '/login'
 
-    # And when I click link "Create Account"
     click_link "Create Account"
 
-    # And I fill in my desired credentials
     fill_in('Name', with: "Corey")
     fill_in('Username', with: 'Cory')
     fill_in('Password', with: 'test')
     fill_in('Password Confirm', with: 'test')
 
-    # And I submit my information
     click_button "Create Account"
 
-    # Then my current page should be "/dashboard"
     expect(current_path).to eq(dashboard_path)
-
-    # And I should see a message in the navbar that says "Logged in as SOME_USER"
     expect(page).to have_content("Logged in as Cory")
-
-    # And I should see my profile information
     expect(page).to have_content("Name: Corey")
-
-    # And I should not see a link for "Login"
     expect(page).to_not have_content("Login")
-
-    # And I should see a link for "Logout"
     expect(page).to have_content("Logout")
   end
 end
