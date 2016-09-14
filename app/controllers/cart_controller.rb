90e63fb593354fcd7b419e6ec1a262d3ffe10e83
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-09-14 14:44:45 -0600

    Implement flash for cart deletion

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,5 +1,5 @@
 class CartController < ApplicationController
-  # include ActionView::Helpers::TextHelper
+  include ActionView::Helpers::TextHelper
 
   def show
     @items = @cart.contained_items
@@ -10,6 +10,12 @@ class CartController < ApplicationController
     @cart.add_item(item.id)
     session[:cart] = @cart.contents
     # flash[:notice] = "You have #{pluralize(x, item.title)} in your cart"
+    redirect_to cart_path
+  end
+
+  def destroy
+    item = Item.find(params[:item_id])
+    flash[:notice] = "Successfully removed #{view_context.link_to item.title, item_path(item)} from your cart."
     redirect_to cart_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :items, only: [:index]
+  resources :items, only: [:index, :show]
 
   # namespace :cart do
   #   resources :items, only: [:create, :index]
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   get '/cart', to: "cart#show"
   post '/cart', to: "cart#update"
+  delete '/cart', to: "cart#destroy"
 
   get '/:category', to: "categories#show"
 
