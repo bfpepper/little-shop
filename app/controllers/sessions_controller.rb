63bd73e020ef61e87ff2854ab55371e491406871
Calaway <Christopher.Calaway@gmail.com>
2016-09-14 19:08:48 -0600

    pass test: Authenticated user logs in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,12 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(username: params[:session][:username])
-    if user.admin? && user.authenticate([:session][:password])
+    # if user.admin? && user.authenticate([:session][:password])
+    #   session[:user_id] = user.id
+    #   redirect_to '/admin'
+    if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      redirect_to '/admin'
-    elsif user && user.authenticate([:session][:password])
-      session[:user_id] = user.id
-      redirect_to user
+      redirect_to dashboard_path(id: user.id)
     else
       render :new
     end
--- spec/features/authenticated_user_can_log_in_spec.rb
+++ spec/features/authenticated_user_can_log_in_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.feature 'Authenticated user logs in' do
   scenario 'they see the dashboard and their username' do
+    user = create(:user)
     # As a visitor
     # When I visit "/"
     visit '/'
@@ -17,20 +18,19 @@ RSpec.feature 'Authenticated user logs in' do
     # And I should see a link to "Create Account"
     expect(page).to have_link 'Create Account'
     # And I fill in my desired credentials
-    fill_in 'Username', with: 'PigeonsAreDoves'
-    fill_in 'Password', with: 'pwd'
+    fill_in 'Username', with: user.username
+    fill_in 'Password', with: user.password
     # And I submit my information
     click_on 'Save Session'
     # Then my current page should be "/dashboard"
     expect(current_path).to eq('/dashboard')
     # And I should see a message in the navbar that says "Logged in as SOME_USER"
-    within ".navbar" do
-      expect(page).to have_content("Logged in as PigeonsAreDoves")
+    within '.navbar' do
+      expect(page).to have_content("Logged in as #{user.username}")
     end
     # And I should see my profile information
     expect(current_path).to eq(dashboard_path)
-    expect(page).to have_content("Logged in as PigeonsAreDoves")
-    expect(page).to have_content("Name: Bob")
+    expect(page).to have_content("Name: #{user.name}")
     # And I should not see a link for "Login"
     expect(page).not_to have_content('Login')
     # And I should see a link for "Logout"
