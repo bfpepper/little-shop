06c08be21843899e2ad38afcc4392aa3caffa8d1
Ben Pepper <bfpepper87@gmail.com>
2016-09-15 16:14:50 -0600

    add third round of card user stories to specs and clean up spec folder.

---
+++ spec/features/user/user_can_see_retired_items_spec.rb
@@ -0,0 +1,4 @@
+# As a user if I visit an item page and that item has been retired
+# Then I should still be able to access the item page
+# And I should not be able to add the item to their cart
+# And I should see in place of the "Add to Cart" button or link - "Item Retired"
---
+++ spec/features/visitor/vistor_cannot_sees_user_info_spec.rb
@@ -0,0 +1,5 @@
+# As an Unuthenticated User
+#   I cannot view another user's private data, such as current order, etc.
+#   I should be redirected to login/create account when I try to check out.
+#   I cannot view the administrator screens or use administrator functionality.
+#   I cannot make myself an administrator.
