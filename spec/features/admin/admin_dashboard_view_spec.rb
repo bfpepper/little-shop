48418f17e41de13f739a48eb5bbeb81b1c2bb515
Ben Pepper <bfpepper87@gmail.com>
2016-09-16 10:33:32 -0600

    cleaned up tests

--- spec/features/admin/admin_dashboard_view_spec.rb
+++ spec/features/admin/admin_dashboard_view_spec.rb
@@ -16,7 +16,6 @@ RSpec.feature 'Admin can log in and redirected to Admin Dashboard' do
   end
 
   scenario 'Registered user does not get to see admin dashboard' do
-    # As a registered user
     user = create(:user)
 
     visit login_path
@@ -26,20 +25,15 @@ RSpec.feature 'Admin can log in and redirected to Admin Dashboard' do
 
     click_on 'Save Session'
 
-    # When I visit "/admin/dashboard"
     visit admin_dashboard_index_path
 
-    # I get a 404
     expect(page).to have_content('404')
     expect(page).to_not have_content('Admin Dashboard')
   end
 
   scenario 'Visitor does not get to see admin dashboard' do
-    # As an unregistered user
-    # When I visit "/admin/dashboard"
     visit admin_dashboard_index_path
 
-    # I get a 404
     expect(page).to have_content('404')
     expect(page).to_not have_content('Admin Dashboard')
   end
