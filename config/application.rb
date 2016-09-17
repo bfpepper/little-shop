f141f18f0edc0589fbff208b8f518414e8b7d903
Ben Pepper <bfpepper87@gmail.com>
2016-09-17 13:26:05 -0600

    add visitor cannot view user or admin pages.

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ Bundler.require(*Rails.groups)
 
 module Littleshop
   class Application < Rails::Application
+    config.exceptions_app = self.routes
     # Settings in config/environments/* take precedence over specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- spec/features/visitor/vistor_cannot_sees_user_info_spec.rb
+++ spec/features/visitor/vistor_cannot_sees_user_info_spec.rb
@@ -1,3 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature 'Visitor cannot see user pages or admin info' do
+  scenario 'Visitor cant see registered users/admin info.' do
+    user = create(:user)
+
+    visit admin_dashboard_index_path
+    expect(page).to have_content('404')
+    
+    visit "/users/#{user.id}"
+    expect(page).to have_content('404')
+  end
+end
+
 # As an Unuthenticated User
 #   I cannot view another user's private data, such as current order, etc.
 #   I should be redirected to login/create account when I try to check out.
