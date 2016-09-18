07ba9121bdcde7b91845213ae82f53a533cd3ee5
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-09-18 14:22:27 -0600

    Fix alignment in dashboard index action

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,10 +2,10 @@ module Admin
   class DashboardController < Admin::BaseController
     def index
       @orders = if params[:filter_by]
-        Order.where(status: params[:filter_by])
-      else
-        Order.all
-      end
+                  Order.where(status: params[:filter_by])
+                else
+                  Order.all
+                end
       @breakdown = Order.breakdown
     end
   end
