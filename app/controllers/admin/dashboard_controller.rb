9bc1b5fb18c4b85fb9a5f769528a8ebf0491807e
Calaway <Christopher.Calaway@gmail.com>
2016-09-19 16:01:31 -0600

    only admin can view admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,8 @@
 module Admin
   class DashboardController < Admin::BaseController
     def index
+      redirect_to dashboard_path unless current_user.admin?
+
       @orders = if params[:filter_by]
                   Order.where(status: params[:filter_by])
                 else
