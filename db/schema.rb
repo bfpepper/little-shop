9cd81ae8cdf07b168076e1a8a5c6c05bbdc95841
Calaway <Christopher.Calaway@gmail.com>
2016-09-20 10:25:45 -0600

    extract user new/edit form to partial

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160916163136) do
+ActiveRecord::Schema.define(version: 20160920155435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20160916163136) do
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
     t.integer  "role",                  default: 0
+    t.string   "phone_number"
   end
 
   add_foreign_key "items", "categories"
