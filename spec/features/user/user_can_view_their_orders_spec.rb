4053f658dd7838d34251cc8f8ed3e49433aaaa92
Nate Anderson <curtislowe10@gmail.com>
2016-09-20 09:53:21 -0600

    Made feature test names consistent, added dropdown on nav, and added sad path on users_can_view_their_orders_spec

--- spec/features/user/user_can_view_their_orders_spec.rb
+++ spec/features/user/user_can_view_their_orders_spec.rb
@@ -18,5 +18,20 @@ RSpec.feature 'A user views their orders' do
   end
 
   scenario "they don't see orders belonging to a different user" do
+    user1 = create(:user_with_orders)
+    user2 = create(:user_with_orders)
+    order_1 = user2.orders.first
+    order_2 = user2.orders.last
+  
+    visit login_path
+  
+    fill_in 'Username', with: user1.username
+    fill_in 'Password', with: user1.password
+    click_on 'Save Session'
+  
+    visit '/orders'
+  
+    expect(page).not_to have_content("Order #{order_1.id}: $#{order_1.price}")
+    expect(page).not_to have_content("Order #{order_2.id}: $#{order_2.price}")
   end
 end
