5843800152a02dfc5970856240403a1e15af5ee1
Calaway <Christopher.Calaway@gmail.com>
2016-09-21 14:39:21 -0600

    built and styled home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def show
+    @items = Item.limit(4).order("RANDOM()")
   end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,11 +2,10 @@ class Cart
   attr_reader :contents
 
   def initialize(initial_contents)
-    @contents = initial_contents || {}
+    @contents = initial_contents || Hash.new(0)
   end
 
   def add_item(item_id)
-    contents[item_id.to_s] ||= 0
     contents[item_id.to_s] += 1
   end
 
